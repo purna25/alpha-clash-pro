e10728ce1596a3377e0d115b7f4278a38d11715f
purna25 <cpurnasree18@gmail.com>
2024-12-12 20:45:11 +0600

    added some elements by javascrip

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,29 @@
+// function play(){
+//   // step-1: hide the home screen to. To hide the home screen add the class hidden to the home section. 
+//   const homeSection = document.getElementById('home-screen');
+//   homeSection.classList.add('hidden');
+
+//   // show the playground
+//   const playgroundSection = document.getElementById('play-ground');
+//   playgroundSection.classList.remove('hidden');
+// }
+
+function continueGame(){
+  // step-1: generate a random alphabet
+  const alphabet = getARandomAlphabet();
+  console.log('your random alphabet', alphabet);
+
+  // set randomly generated alphabet to the screen (show it)
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  currentAlphabetElement.innerText = alphabet;
+
+  // set background color
+  setBackgroundColorById(alphabet);
+
+}
+
+function play(){
+  hideElementById('home-screen');
+  showElementById('play-ground');
+  continueGame();
+}
